1829412e77fca432ba885c77b90b57d7689e85e7
theko2fi <72862222+theko2fi@users.noreply.github.com>
2023-02-02 22:00:43 +0100

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -16,15 +16,18 @@ publicIpFromInterface() {
 }
 
 publicIpFromMetadata() {
-  if curl -s http://169.254.169.254/metadata/v1/vendor-data | grep DigitalOcean >/dev/null; then
-    ENDPOINT="$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)"
-  elif test "$(curl -s http://169.254.169.254/latest/meta-data/services/domain)" = "amazonaws.com"; then
-    ENDPOINT="$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)"
-  elif host -t A -W 10 metadata.google.internal 127.0.0.53 >/dev/null; then
-    ENDPOINT="$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip")"
-  elif test "$(curl -s -H Metadata:true --noproxy '*' 'http://169.254.169.254/metadata/instance/compute/provider/?api-version=2021-02-01&format=text')" = "Microsoft.Compute"; then
-    ENDPOINT="$(curl -H Metadata:true --noproxy '*' 'http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress/?api-version=2021-02-01&format=text')"
+  if test "$(cloud-init query cloud-name)" = "multipass"; then
+    publicIpFromInterface
   fi
+#   if curl -s http://169.254.169.254/metadata/v1/vendor-data | grep DigitalOcean >/dev/null; then
+#     ENDPOINT="$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)"
+#   elif test "$(curl -s http://169.254.169.254/latest/meta-data/services/domain)" = "amazonaws.com"; then
+#     ENDPOINT="$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)"
+#   elif host -t A -W 10     publicIpFromInterfacemetadata.google.internal 127.0.0.53 >/dev/null; then
+#     ENDPOINT="$(curl -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip")"
+#   elif test "$(curl -s -H Metadata:true --noproxy '*' 'http://169.254.169.254/metadata/instance/compute/provider/?api-version=2021-02-01&format=text')" = "Microsoft.Compute"; then
+#     ENDPOINT="$(curl -H Metadata:true --noproxy '*' 'http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress/?api-version=2021-02-01&format=text')"
+#   fi
 
   if echo "${ENDPOINT}" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"; then
     export ENDPOINT="${ENDPOINT}"
